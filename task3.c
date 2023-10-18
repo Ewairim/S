fa11f804a5e159dee4a73bde2f0ec6dd5540d7b4
Brendah90 <bregamah@gmail.com>
2023-10-18 13:01:57 -0700

    task3.c

--- task3.c
+++ task3.c
@@ -1,82 +1,92 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include "shell.h"
 
 /**
- * _env - displays the existing environment variables
- * Return: 0 on success, 1 on error
+ * _myenvironment - prints the current environment
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
  */
-int _env(ino_t *info)
+int _myenvironment(info_t *info)
 {
-	char user_input[256]; 
-	char *args[128]; 
-	char *path = getenv("PATH");
-
-	while (1)
-	{
-		write(STDOUT_FILENO, "info->env: ", 16);
-		if (fgets(user_input, sizeof(user_input), stdin) == NULL)
-			break;
-
-		user_input[strcspn(user_input, "\n")] = '\0';
-
-		int num_args = 0;
-		char *_getenv(ino_t *info, const char *name)
-
-		while (token != NULL)
-		{
-			args[num_args++] = token;
-			token = strtok(NULL, " ");
-		}
-		char *args[128]; = NULL;
-
-		char *cmd_path = NULL;
-		int exists = 0;
-		char *token; = strtok(path, ":");
-
-		while (token != NULL)
-		{
-			char full_path[256]; 
-
-			strcat(strcat(full_path, token), "/");
-			if (access(full_path, X_OK) == 0)
-			{
-				cmd_path = strdup(full_path);
-				exists = 1;
-				break;
-			}
-			token = strtok(NULL, ":");
-		}
-
-		if (exists)
-		{
-			pid_t pid = fork();
-
-			if (pid == -1)
-				perror("fork");
-			else if (pid == 0)
-			{
-				execve(cmd_path, args, NULL);
-				perror("execve");
-				exit(1);
-			}
-			else
-			{
-				int status;
-
-				waitpid(pid, &status, 0);
-				if (WIFEXITED(status) && WEXITSTATUS(status) == 1)
-					write(STDOUT_FILENO, "Error: Execution failed\n", 23);
-			}
-		}
-		else
-			write(STDOUT_FILENO, "\n", 1);
-		free(cmd_path);
-	}
+	print_list_str(info->env);
 	return (0);
 }
 
+/**
+ * _getenv - gets the value of an environ variable
+ * @info: Structure containing potential arguments. Used to maintain
+ * @name: env var name
+ *
+ * Return: the value
+ */
+char *_getenv(info_t *info, const char *name)
+{
+	list_t *node = info->env;
+	char *p;
 
+	while (node)
+	{
+		p = starts_with(node->str, name);
+		if (p && *p)
+			return (p);
+		node = node->next;
+	}
+	return (NULL);
+}
+
+/**
+ * _mysetenv - Initialize a new environment variable,
+ *             or modify an existing one
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ *  Return: Always 0
+ */
+int _mysetenv(info_t *info)
+{
+	if (info->argc != 3)
+	{
+		_eputs("Incorrect number of arguements\n");
+		return (1);
+	}
+	if (_setenv(info, info->argv[1], info->argv[2]))
+		return (0);
+	return (1);
+}
+
+/**
+ * _myunsetenv - Remove an environment variable
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ *  Return: Always 0
+ */
+int _myunsetenv(info_t *info)
+{
+	int i;
+
+	if (info->argc == 1)
+	{
+		_eputs("Too few arg.\n");
+		return (1);
+	}
+	for (i = 1; i <= info->argc; i++)
+		_unsetenv(info, info->argv[i]);
+
+	return (0);
+}
+
+/**
+ * populate_env_list - populates env linked list
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
+ */
+int populate_env_list(info_t *info)
+{
+	list_t *node = NULL;
+	size_t i;
+
+	for (i = 0; environ[i]; i++)
+		add_node_end(&node, environ[i], 0);
+	info->env = node;
+	return (0);
+}
