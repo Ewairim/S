526c779ead86788e088a50d50dc0eef76911c5e4
Brendah90 <bregamah@gmail.com>
2023-10-18 13:10:49 -0700

    task2.c

--- task2.c
+++ task2.c
@@ -1,91 +1,43 @@
 #include "shell.h"
 
 /**
- * _myenvironment - prints the current environment
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- * Return: Always 0
+ * _print_errors - prints errors to stdin
+ * @pg_name: program name
+ * @cmd_name: command name
+ * @process_no: process number
  */
-int _myenvironment(info_t *info)
+void _print_error(string pg_name, string cmd_name, int process_no)
 {
-	print_list_str(info->env);
-	return (0);
+	char process[6];
+
+	number_to_string(process, process_no);
+	_ewrite(pg_name);
+	_ewrite(": ");
+	_ewrite(process);
+	_ewrite(": ");
+	_ewrite(cmd_name);
+	_ewrite(": not found\n");
 }
 
 /**
- * _getenv - gets the value of an environ variable
- * @info: Structure containing potential arguments. Used to maintain
- * @name: env var name
- *
- * Return: the value
+ * _print_atoi_error - prints errors to stdin
+ * @pg_name: program name
+ * @cmd_arguments: array of user command and arguments
+ * @process_no: process number
  */
-char *_getenv(info_t *info, const char *name)
+void _print_atoi_error(string pg_name, array cmd_args, int process_no)
 {
-	list_t *node = info->env;
-	char *p;
+	char process[4];
 
-	while (node)
-	{
-		p = starts_with(node->str, name);
-		if (p && *p)
-			return (p);
-		node = node->next;
-	}
-	return (NULL);
+	number_to_string(process, process_no);
+	_ewrite(pg_name);
+	_ewrite(": ");
+	_ewrite(process);
+	_ewrite(": ");
+	_ewrite(cmd_args[0]);
+	_ewrite(": invalid number: ");
+	_ewrite(cmd_args[1]);
+	_ewrite("\n");
 }
 
-/**
- * _mysetenv - Initialize a new environment variable,
- *             or modify an existing one
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- *  Return: (0)
- */
-int _mysetenv(info_t *info)
-{
-	if (info->argc != 3)
-	{
-		_eputs("Incorrect number of arguements\n");
-		return (1);
-	}
-	if (_setenv(info, info->argv[1], info->argv[2]))
-		return (0);
-	return (1);
-}
 
-/**
- * _myunsetenv - Remove an environment variable
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- *  Return: Always 0
- */
-int _myunsetenv(info_t *info)
-{
-	int i;
-
-	if (info->argc == 1)
-	{
-		_eputs("Too few arg.\n");
-		return (1);
-	}
-	for (i = 1; i <= info->argc; i++)
-		_unsetenv(info, info->argv[i]);
-
-	return (0);
-}
-
-/**
- * populate_env_list - populates env linked list
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- * Return: Always 0
- */
-int populate_env_list(info_t *info)
-{
-	list_t *node = NULL;
-	size_t i;
-
-	for (i = 0; environ[i]; i++)
-		add_node_end(&node, environ[i], 0);
-	info->env = node;
-	return (0);
