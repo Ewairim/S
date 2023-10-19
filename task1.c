838b2bca96f982eaab39b6951a870fcbd27b89ac
Brendah90 <bregamah@gmail.com>
2023-10-19 00:26:00 -0700

    task1.c

--- task1.c
+++ task1.c
@@ -1,35 +1,22 @@
 #include "shell.h"
 
-/**
- * custom_exit - Exits the shell
- * @param_count: Number of parameters
- * @arg_array: Array containing command-line arguments
- * @envp: Array of environment variables
- *  Returns: Terminates with a specified exit condition*/
-int main(int argc, char **argv, char **envp)
-{
+int main(int argc, char **argv, char **envp) {
     char *line = NULL;
     size_t len = 0;
     ssize_t nread;
     char **args = NULL;
-    int status = 0;
 
     /* Ignore the arguments */
     (void)argc;
     (void)argv;
-    // Declaration of the signal_handler function
-    (void)ignal_handler(int signum);
-
 
     /* Print the prompt and wait for user input */
     signal(SIGINT, signal_handler); /* Handle Ctrl+C */
-    while (1)
-    {
+    while (1) {
         write(STDOUT_FILENO, "537sh$ ", 7);
         fflush(stdout);
         nread = getline(&line, &len, stdin);
-        if (nread == -1) /* Handle Ctrl+D or error */
-        {
+        if (nread == -1) { /* Handle Ctrl+D or error */
             if (errno)
                 perror("getline");
             else
@@ -38,21 +25,20 @@ int main(int argc, char **argv, char **envp)
         }
         line[nread - 1] = '\0'; /* Remove the newline character */
 
-        /* Parse the line into arguments */
-        args = split_line(line);
-        if (args == NULL)
-            continue;
+        args = split_line(line); /* Split the line into arguments */
+        if (args == NULL) {
+            continue; // Handle empty input or other errors
+        }
 
-        /* Execute the command with the arguments */
-        status = execute_command(args, envp);
+        if (execve(args[0], args, envp) == -1) {
+            perror("execve");
+            free(args);
+            return -1;
+        }
 
-        /* Free the memory allocated for the arguments */
-       arg_array = info->argv ;
+        free(args); /* Free the memory allocated for the arguments */
     }
 
-    /* Free the memory allocated for the line */
-    free(line);
-
-    return (0);
+    free(line); /* Free the memory allocated for the line */
+    return 0;
 }
-
