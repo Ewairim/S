27a2c18322cbe0e0478a2430ef9453689f75f670
Ewairim <kihumbaeunice10@gmail.com>
2023-10-19 05:42:58 -0400

    header file

---
+++ shell.h
@@ -0,0 +1,11 @@
+#ifndef SHELL_H
+#define SHELL_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
+#include <stddef.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#endif
